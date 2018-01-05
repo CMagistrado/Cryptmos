e2d2eab2b714ba662c4fc8c8a8ffc162ef48800d
pump <prob@bably.pointless>
2018-01-05 13:10:01 -0800

    Need to create a while (data):

--- hist.py
+++ hist.py
@@ -7,16 +7,25 @@ import requests, json, time, sys
 class Ohmu:
 
     def download(self, coin):
-        coin = coin.replace("\"", "")
-        coin = coin.replace("\n", "")
-        url = "https://min-api.cryptocompare.com/data/histoday?fsym=" + coin + "&tsym=USD&limit=20000&aggregate=1&toTs=1515132816&e=CCCAGG"
-        r = requests.get(url)
-        parsedJSON = json.loads(r.text)
-
         # Create and open coinFile
         fopen = open(coin, "a")
-        fopen.write(str(parsedJSON))
-        print(coin + " written.")
-        fopen.close()
+        
+        coin = coin.replace("\"", "")
+        coin = coin.replace("\n", "")
+        url = "https://min-api.cryptocompare.com/data/histoday?fsym=" + coin + "&tsym=USD&limit=20000&aggregate=1&toTs=1507917600&e=CCCAGG"
+        r = requests.get(url)
+        parsedJSON = json.loads(r.text)
+        
+        data = parsedJSON["Data"]
+    
+        if data:
+            # Remove the '[' from data
+            data = str(data)
+            data = data[1:-1] + ", "
+            fopen.write(data)
+            print("[" + str(timeFrom) + "] written.")
+            self.download(coin)
 
+        fopen.close()
+        print(coin + " written.")
         return 
